7ae27fc258c0b0a334bd4dfc960204382f6139b8
Sergej Atamantschuk <sergejatam@gmail.com>
2025-06-22 15:40:22 +0200

    add website tool

--- src/real_estate_intelligence_unit/crew.py
+++ src/real_estate_intelligence_unit/crew.py
@@ -2,9 +2,14 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from crewai_tools import ScrapeWebsiteTool
+
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+website_tool = WebsiteSearchTool()
 
 @CrewBase
 class RealEstateIntelligenceUnit():
@@ -23,14 +28,18 @@ class RealEstateIntelligenceUnit():
     def senior_property_scout(self) -> Agent:
         return Agent(
             config=self.agents_config['senior_property_scout'], # type: ignore[index]
-            verbose=True
+            verbose=True,
+            reasoning=False,
+            tools=[website_tool],
         )
 
     @agent
     def real_estate_analyst(self) -> Agent:
         return Agent(
             config=self.agents_config['real_estate_analyst'], # type: ignore[index]
-            verbose=True
+            verbose=True,
+            reasoning=False,
+            tools=[website_tool],
         )
 
     # To learn more about structured task outputs,
